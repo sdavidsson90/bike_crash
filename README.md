d132d39b6f829eaeda1d8741106ae0017d1df9b6
Sighvatur Sveinn Davidsson <sdavidsson90@gmail.com>
2021-11-16 22:14:23 +0100

    updates 16/11 evening

--- README.md
+++ README.md
@@ -6,9 +6,13 @@ This is a data analytics project on bike crashes (with cars) in Chapel Hill, Nor
 https://www.kaggle.com/atharvaingle/bikepedcrash?select=BikePedCrash.csv
 
 [2] Original source is (as noted by kaggle user):
+Description: Bicyclist and pedestrian crashes that occurred in North Carolina between years 2007 through 2019
+
 https://opendata-townofchapelhill.hub.arcgis.com/datasets/NCDOT::bicycle-crashes-2007-to-2019/explore
 
 [3] Equally interesting pedestrian crashes: 
+Description: Bicyclist and pedestrian crashes that occurred in North Carolina between years 2007 through 2019
+
 https://opendata-townofchapelhill.hub.arcgis.com/datasets/NCDOT::pedestrian-crashes-2007-to-2019/explore?location=35.132540%2C-79.817750%2C7.51
 
 [4] More data can be found at:
--- code/bike_crash1.R
+++ code/bike_crash1.R
@@ -2,83 +2,72 @@
 pacman::p_load(tidyverse,   # 
                tidylog,    # 
                vroom,
-               tidymodels,
-               collapse,
-               sf,
-               tmap
-) 
+               lubridate,
+               collapse) 
 
 options(scipen = 999)
 
 ##########################
 # Indlæsning af data 
-bike_crash <- vroom("raw_data/bike_crash.csv") 
+bike_crash <- vroom("raw_data/bike_crash (ch open data hub).csv") 
 colnames(bike_crash) <- janitor::make_clean_names(colnames(bike_crash))
 
-# https://opendata-townofchapelhill.hub.arcgis.com/datasets/NCDOT::bicycle-crashes-2007-to-2019/explore?location=35.130408%2C-79.817750%2C7.48
+# _ _ _ _
+# DATAVASK
 
-# https://opendata-townofchapelhill.hub.arcgis.com/search?tags=transportation%2Cmovement
+# NA-ballade: Vi ensretter alle missings med NA
+    bike_crash <- bike_crash %>% mutate(across(where(is_character), ~str_replace(., pattern = "\\.|Missing|Unknown|Unknown/Missing|999", replacement = NA_character_)))
+    
+# RELEVELING
+    # Her kan man godt vælge, at lave en generel funktion. 
+    # Men en god programmør ved også at man nogle gange skal springe over hvor gærdet er lavest. TIME IS MONEY
+    bike_lvl  <- c("0-5", "6-10", "11-15", "16-19", "20-24", "25-29", "30-39", "40-49", "50-59", "60-69", "70+")
+    drvr_lvl  <- c("0-19", "20-24", "25-29", "30-39", "40-49", "50-59", "60-69", "70+")
+    light_lvl <-  c("Dawn", "Daylight", "Dusk", "Dark - Lighted Roadway", "Dark - Unknown Lighting", "Dark - Roadway Not Lighted", "Other")
+    loc_lvl   <- c("Rural (<30% Developed)", "Mixed (30% To 70% Developed)", "Urban (>70% Developed)")
+    speed_lvl <- c("5 - 15 MPH", "20 - 25  MPH", "30 - 35  MPH" , "40 - 45  MPH", "50 - 55  MPH", "60 - 75 MPH")
+    
+    bike_crash %>% 
+      mutate(bike_age_grp = factor(bike_age_grp, levels =  bike_lvl),
+             drvr_age_grp = factor(drvr_age_grp, levels =  drvr_lvl),
+             light_cond   = factor(light_cond,   levels = light_lvl),
+             locality     = factor(locality,     levels = loc_lvl  ),
+             speed_limit  = factor(speed_limit,  levels = speed_lvl))
+    
+    rm(bike_lvl, drvr_lvl, light_lvl, loc_lvl, speed_lvl)
 
-# # # NA-ballade
-# Vi laver forskellige typer missings om til NA (Det her må kunne gøres smartere på én sætning)
-bike_crash <- bike_crash %>% mutate(across(where(is_character), ~na_if(., ".")))
-bike_crash <- bike_crash %>% mutate(across(where(is_character), ~na_if(., "Missing")))
-bike_crash <- bike_crash %>% mutate(across(where(is_character), ~na_if(., 999)))
-bike_crash <- bike_crash %>% mutate(across(where(is_character), ~na_if(., "Unknown")))
-bike_crash <- bike_crash %>% mutate(across(where(is_character), ~na_if(., "Unknown/Missing")))
-
-
-
-# kontrol - Vi prøver på én sætning
-str_count
-
-
-??use_github
-
-# Note:
-# Man kan også bruge map_dfr, men den er dobbelt så langsom
-# map_dfr(bike_crash, ~na_if(.x, ".")) 
-
-# # # RELEVELING
-
-# 1) 
-# Her kan man godt vælge, at lave en generel funktion. Men en god programmør ved også at man nogle gange skal springe over hvor gærdet er lavest. TIME IS MONEY
-bike_lvl  <- c("0-5", "6-10", "11-15", "16-19", "20-24", "25-29", "30-39", "40-49", "50-59", "60-69", "70+")
-drvr_lvl  <- c("0-19", "20-24", "25-29", "30-39", "40-49", "50-59", "60-69", "70+")
-light_lvl <-  c("Dawn", "Daylight", "Dusk", "Dark - Lighted Roadway", "Dark - Unknown Lighting", "Dark - Roadway Not Lighted", "Other")
-loc_lvl   <- c("Rural (<30% Developed)", "Mixed (30% To 70% Developed)", "Urban (>70% Developed)")
-speed_lvl <- c("5 - 15 MPH", "20 - 25  MPH", "30 - 35  MPH" , "40 - 45  MPH", "50 - 55  MPH", "60 - 75 MPH")
-
-bike_crash %>% 
-  mutate(bike_age_grp = factor(bike_age_grp, levels =  bike_lvl),
-         drvr_age_grp = factor(drvr_age_grp, levels =  drvr_lvl),
-         light_cond   = factor(light_cond,   levels = light_lvl),
-         locality     = factor(locality,     levels = loc_lvl  ),
-         speed_limit  = factor(speed_limit,  levels = speed_lvl))
-
-rm(bike_lvl, drvr_lvl, light_lvl, loc_lvl, speed_lvl)
-
-# 2)
 # DATO / TID VARIABLE. 
-bike_crash$crash_year <- factor(bike_crash$crash_year, levels = sort(unique(bike_crash$crash_year)))
-bike_crash$crash_month <- factor(bike_crash$crash_month, levels = unique(bike_crash$crash_month))
-bike_crash$crash_day <- factor(bike_crash$crash_day, levels = c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
-bike_crash$crash_hour <- factor(bike_crash$crash_hour, levels = sort(unique(bike_crash$crash_hour)))
+    bike_crash$crash_year <- factor(bike_crash$crash_year, levels = sort(unique(bike_crash$crash_year)))
+    bike_crash$crash_month <- factor(bike_crash$crash_month, levels = unique(bike_crash$crash_month))
+    bike_crash$crash_day <- factor(bike_crash$crash_day, levels = c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
+    bike_crash$crash_hour <- factor(bike_crash$crash_hour, levels = sort(unique(bike_crash$crash_hour)))
+    
+    # Vi har ikke månedsdag, og kan derfor ikke kode dem som nøjagtig datetime. Vi bruger bare den 1. i en måned (assignes automatisk)
+    bike_crash$crash_date <- ym(str_c(bike_crash$crash_year, "-", bike_crash$crash_month))
 
 # # # Mærkeligt alder 70 + 
+    # Det ser vi lige på om vi skal gøre noget ved senere
 bike_crash %>% count(drvr_age, drvr_age_grp) %>% print(n = nrow(.))
-funique(bike_crash$drvr_age) %>% system.time
 
 # ______________________________________________
 # 
 # EDA
 
-# glimpse(bike_crash)
-# bike_crash %>% count(crash_year) %>% ggplot(aes(x = crash_year, y = n)) + geom_bar(stat = "identity")
-# bike_crash %>% count(crash_month) %>% ggplot(aes(x = crash_month, y = n)) + geom_bar(stat = "identity")
-# bike_crash %>% count(crash_hour) %>% ggplot(aes(x = crash_hour, y = n)) + geom_bar(stat = "identity")
-# bike_crash %>% count(crash_day) %>% ggplot(aes(x = crash_day, y = n)) + geom_bar(stat = "identity")
+glimpse(bike_crash)
+bike_crash %>% count(crash_year) %>% ggplot(aes(x = crash_year, y = n)) + geom_bar(stat = "identity")
+bike_crash %>% count(crash_month) %>% ggplot(aes(x = crash_month, y = n)) + geom_bar(stat = "identity")
+bike_crash %>% count(crash_hour) %>% ggplot(aes(x = crash_hour, y = n)) + geom_bar(stat = "identity")
+bike_crash %>% count(crash_day) %>% ggplot(aes(x = crash_day, y = n)) + geom_bar(stat = "identity")
 
+
+
+bike_crash %>% 
+  group_by(crash_year) %>% 
+  count(crash_month) %>% 
+  ungroup() %>% 
+  ggplot(aes(x = crash_month, y = n, group = crash_year)) + geom_line(alpha = 0.3)
+
+summ
 
 # Modeling
 colnames(bike_crash)
